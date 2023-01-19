efbba5c0cecdf29311d3da3b4683d53b1ace227e
wonhj <wonhjteacher@gmail.com>
2023-01-19 11:43:28 +0900

    완료

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
+const API_KEY = `api 키 넣기`;
 let newsData = [];
 let topics = document.querySelectorAll('.gnb>li');
 topics.forEach(ele=>ele.addEventListener('click',(e)=>getTopic(e)));
 let searchBtn = document.querySelector('.search-btn');
+let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=5`) ;
+
+const getNews = async() => {
+    let header = new Headers({'x-api-key':API_KEY})
+    let response = await fetch(url,{headers: header})
+    let data =   await response.json() ; 
+    newsData = data.articles;
+    viewRender();
+}
+
 //검색어 입력 
-const getSearchNews = async() => {
+const getSearchNews = () => {
     let searchKeyword = document.querySelector('#search').value ;
     console.log(searchKeyword)
-    let url = new URL(`https://api.newscatcherapi.com/v2/search?q=${searchKeyword}&page_size=5`);
-    let header = new Headers({'x-api-key':'본인 api 키 지정 '})
-    let response = await fetch(url,{headers:header}) 
-    let data =   await response.json() ; 
-    newsData = data.articles;
-    viewRender();
+    url = new URL(`https://api.newscatcherapi.com/v2/search?q=${searchKeyword}&page_size=5`);
+    getNews()
 }
 //메뉴 클릭시 
-const getTopic = async(e) =>{
+const getTopic = (e) =>{
     let topic = e.target.dataset.topic;
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=5`);
-    let header = new Headers({'x-api-key':'본인 api 키 지정 '})
-    let response = await fetch(url,{headers:header}) 
-    let data =   await response.json() ; 
-    newsData = data.articles;
-    console.log(newsData)
-    viewRender();
-
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=5`);
+    getNews()
 }
 //최근 해당 토픽 해드라인 뉴스 가져오기 
-const getLatestHeadlines = async() => {
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=5`);
-    let header = new Headers({'x-api-key':'본인 api 키 지정 '})
-    let response = await fetch(url,{headers:header}) 
-    let data =   await response.json() ; 
-    newsData = data.articles;
-    console.log(newsData)
-    viewRender();
+const getLatestHeadlines = () => {
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=5`); 
+    getNews()
 }
 
 //가져온 뉴스를 화면에 렌더 하는 함수 
@@ -52,7 +48,7 @@ const viewRender = () => {
     }).join("");
     
     document.querySelector('.news-board').innerHTML = newsDataResult ;
-
 }
+
 getLatestHeadlines();
 searchBtn.addEventListener('click',getSearchNews)
